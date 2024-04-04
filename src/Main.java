eb8a2ae9ede4bfcf3221bb6d105ee7713c501585
Hidalgo87 <juanfgomez6@gmail.com>
2024-04-04 13:57:23 -0500

    arreglillos

--- src/Main.java
+++ src/Main.java
@@ -19,70 +19,79 @@ public class Main {
         List<String> pasatiempo = new ArrayList<>();
         String nickname_user1 = "cristianf";
         String password_user1 = "abcd1234";
-        System.out.println("LOGIN BOOKING");
-        System.out.println("---------------------------------");
-        System.out.println("Ingrese su usuario: ");
-        String user = scanner.nextLine();
-        System.out.println("Ingrese su contraseña: ");
-        String password = scanner.nextLine();
+        while (true) {
+            System.out.println("LOGIN BOOKING");
+            System.out.println("---------------------------------");
+            System.out.println("Ingrese su usuario: ");
+            String user = scanner.nextLine();
+            System.out.println("Ingrese su contraseña: ");
+            String password = scanner.nextLine();
 
-        if (user.equals(nickname_user1) && password.equals(password_user1)) {
-            while(true){
-                System.out.println("BOOKING MENU");
-                System.out.println("---------------------------------");
-                System.out.println("1. Ingresar hobbies");
-                System.out.println("2. Ver recomendaciones segun mis hobbies");
-                int opcion = scanner.nextInt();
-                scanner.nextLine();
-                if (opcion == 1){
-                    while(true) {
-                        System.out.println("---------------------------------");
-                        System.out.print("Ingresa tus Hobbies: ");
-                        String hobby = scanner.nextLine();
-                        System.out.println("Presione la letra 's' en caso de que no tenga más hobbies");
+            if (user.equals(nickname_user1) && password.equals(password_user1)) {
+                while (true) {
+                    System.out.println("BOOKING MENU");
+                    System.out.println("---------------------------------");
+                    System.out.println("1. Ingresar hobbies");
+                    System.out.println("2. Ver recomendaciones segun mis hobbies");
+                    int opcion = scanner.nextInt();
+                    scanner.nextLine();
+                    if (opcion == 1) {
+                        while (true) {
+                            System.out.println("---------------------------------");
+                            System.out.print("Ingresa tus Hobbies: ");
+                            String hobby = scanner.nextLine();
+                            System.out.println("Presione la letra 's' en caso de que no tenga más hobbies");
 
-                        // Verificar si se presiona la tecla 'q' para salir
-                        if (hobby.equalsIgnoreCase("s")) {
-                            break;
-                        }
-                        hobbies.add(hobby);
-                    }
-
-                } else if (opcion == 2) {
-                    if(hobbies.isEmpty()){
-                        System.out.println("No has ingresado hobbies");
-                    }else {
-                        System.out.println("Que lugares quieres que te muestre según tus hobbies:");
-                        System.out.println("Tus hobbies:");
-                        int contador = 1;
-                        for (String hobbie : hobbies) {
-                            System.out.println(contador + "." + hobbie);
-                            contador = contador + 1;
-                        }
-                        while(true) {
-                            int indice = scanner.nextInt();
-                            if (indice == 0 ) {
+                            // Verificar si se presiona la tecla 'q' para salir
+                            if (hobby.equalsIgnoreCase("s")) {
                                 break;
-                            }else{
-                            pasatiempo.add(hobbies.get(indice - 1));
-                            System.out.println("En caso de no agregar mas pulse '0'");
                             }
+                            hobbies.add(hobby);
                         }
-                        String apiKey = "AIzaSyA-dg5_nxz03QMOcCgRuXuQdCd3iAkNmDU";
-                        double[] userLocation = getLocation();
-                        searchPlacesForHobbies(userLocation[0], userLocation[1], apiKey, pasatiempo);
-                        //scanner.close();
+
+                    } else if (opcion == 2) {
+                        if (hobbies.isEmpty()) {
+                            System.out.println("No has ingresado hobbies");
+                        } else {
+                            System.out.println("Que lugares quieres que te muestre según tus hobbies:");
+                            System.out.println("Tus hobbies:");
+                            int contador = 1;
+                            for (String hobbie : hobbies) {
+                                System.out.println(contador + "." + hobbie);
+                                contador = contador + 1;
+                            }
+                            pasatiempo = new ArrayList<>();
+                            while (true) {
+                                int indice = scanner.nextInt();
+                                if (indice == 0) {
+                                    break;
+                                } else {
+                                    if (pasatiempo.contains(hobbies.get(indice - 1))){
+                                        System.out.println("[WARNING] Ya lo seleccionaste anteriormente, intenta con otro");
+                                    }
+                                    else{
+                                        pasatiempo.add(hobbies.get(indice - 1));
+                                    }
+
+                                    System.out.println("En caso de no agregar mas pulse '0'");
+                                }
+                            }
+                            String apiKey = "AIzaSyA-dg5_nxz03QMOcCgRuXuQdCd3iAkNmDU";
+                            double[] userLocation = getLocation();
+                            searchPlacesForHobbies(userLocation[0], userLocation[1], apiKey, pasatiempo);
+                            //scanner.close();
+                        }
+
+                    } else {
+                        System.out.println("Comando Invalido");
                     }
 
-                }else{
-                    System.out.println("Comando Invalido");
                 }
-
+            } else {
+                System.out.println("El usuario no existe");
             }
-        }else {
-            System.out.println("El usuario no existe");
         }
-        scanner.close();
+
     }
 
     public static double[] getLocation() {
